34fa843ac5c9221a74f9732f9d9b1a96887af8e0
Yolanda Haynes <yhaynes@giyo.us>
2021-03-15 23:33:44 -0700

    Add files via upload

---
+++ squashTheBugs.js
@@ -0,0 +1,15 @@
+//Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// 
+function findLongest(str) {
+  
+    var spl = str.split(" ");
+    var longest = 0
+    
+    for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+       longest = spl[i].length  
+      }
+      }
+      return longest
+  }
